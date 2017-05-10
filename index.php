f16a315772ee31630bed31ab4bc8d4c9440e4363
Colin McKean <ezikill@gmail.com>
2017-05-10 16:28:43 +0000

    add table tags to format as table

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ $query = "SELECT * FROM users";
 $result = mysqli_query($db, $query);
 
 if (mysqli_num_rows($result) > 0) {
+    echo "<table><tr><td>ID</td><th>Username</th><th>email</th></tr>";
     while($row = mysqli_fetch_assoc($result)) {
-        echo $row["id"] . " " .$row["username"] . " " . $row["email"] . "<br>";
+        echo "<tr><td>".  $row["id"] . "</td><td>" .$row["username"] . "</td><td>" . $row["email"] . "</td><tr>";
     }
+    echo "</table>";
 } else {
     echo "NO DATA";
 }
