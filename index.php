f0b92e50e8584ad73533a6db95e6127aedb8300c
Colin McKean <ezikill@gmail.com>
2017-05-10 16:23:06 +0000

    add basic retrival of data from database

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
 <?php include('connection.php'); ?>
+<?php
+
+$query = "SELECT * FROM users";
+$result = mysqli_query($db, $query);
+
+if (mysqli_num_rows($result) > 0) {
+    while($row = mysqli_fetch_assoc($result)) {
+        echo $row["id"] . " " .$row["username"] . " " . $row["email"] . "<br>";
+    }
+} else {
+    echo "NO DATA";
+}
+
+mysqli_close($db);
+?>
